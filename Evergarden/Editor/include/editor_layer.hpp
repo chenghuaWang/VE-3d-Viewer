5ec45bda13e535fbf42b975819dd602622c69e9f
Chenghua <68260701+chenghuaWang@users.noreply.github.com>
2022-04-27 22:37:31 +0800

    Create editor_layer.hpp

---
+++ Evergarden/Editor/include/editor_layer.hpp
@@ -0,0 +1,32 @@
+/*!
+ * @author  chenghua.wang
+ * @file    Evergarden/Editor/include/editor_layer.hpp
+ * @brief   The most basic class for all layers.
+ */
+
+#ifndef H_EDITOR_LAYER
+#define H_EDITOR_LAYER
+
+#ifdef WIN32
+#pragma once
+#endif //! WIN32
+
+namespace evergarden {
+
+    class Layer {
+    public:
+
+    private:
+
+    }; //! class Layer
+
+    class LayerStack {
+    public:
+    
+    private:
+
+    }; //! class LayerStack
+
+}//! namespace evergarden
+
+#endif //! H_EDITOR_LAYER
