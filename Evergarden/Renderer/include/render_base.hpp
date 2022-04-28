c11b44178e9590494fefa363d383d51425b711d7
Chenghua <68260701+chenghuaWang@users.noreply.github.com>
2022-04-28 23:23:10 +0800

    Create render_base.hpp

---
+++ Evergarden/Renderer/include/render_base.hpp
@@ -0,0 +1,28 @@
+/*!
+ * @author  chenghua.wang
+ * @file    Evergarden/Renderer/include/render_base.hpp
+ * @brief   Base file for Renderer. Provide macros and basic functions
+ * 
+ * @details We packagged OpenGL API in this Class-like interface.
+ */
+
+#ifndef H_RENDER_BASE
+#define H_RENDER_BASE
+
+#ifdef WIN32
+#pragma once
+#endif //! WIN32
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+
+namespace evergarden {
+
+}
+
+#endif
