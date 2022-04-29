18df5f708d6bd498fe920fbd4c2f43599a5c1815
Chenghua <68260701+chenghuaWang@users.noreply.github.com>
2022-04-29 17:56:57 +0800

    Update Application.hpp

--- Evergarden/App/include/Application.hpp
+++ Evergarden/App/include/Application.hpp
@@ -11,37 +11,44 @@
 #pragma once
 #endif //! WIN32
 
-#include "Editor/include/app_window.hpp"
-#include "Editor/include/editor_layer.hpp"
+#include "Editor/include/editor_core.hpp"
+#include "Core/include/core_time.hpp"
 
 namespace evergarden {
 
-    class App {
+    class __declspec(dllexport) App {
     public:
-        App(std::string window_name);
+        App(std::string window_name = "Evergarden");
         ~App();
 
     public:
-        void run();
-        void onEvent(Event &e);
-        void pushLayer(Layer* rhs);
-        void popLayer(Layer* rhs);
-        void shutdown();
+        void                        run();
+        void                        onEvent(Event &e);
+        void                        pushLayer(Layer* rhs);
+        void                        popLayer(Layer* rhs);
+        bool                        shutdown();
 
-    EVENT_CALL_BACK:
-        ///< This call back just for normal Application Event.
-        bool __on_app_window_resize__(WindowCloseEvent &e);
-        bool __on_app_window_close__(WindowCloseEvent &e);
+    public:
+        static App*                 create(); 
+        static App*                 getInstance() { return Instance; };
+
+    public:
+        static App*                 Instance;
     
     private:
         std::shared_ptr<MainWindow> m_window;
-        static App*                 Instance;
         ImGuiLayer*                 m_imgui_initial_layer;
         LayerStack                  m_layer_stack;
         bool                        m_window_run = true;
         bool                        m_render_run = true;
         bool                        m_window_minimize = true;
         float                       m_last_frame_time_cnt = 0.f;
+        TimeFrame                   m_time_frame;
+
+    EVENT_CALL_BACK:
+        ///< This call back just for normal Application Event.
+        bool __on_app_window_resize__(WindowResizeEvent &e);
+        bool __on_app_window_close__(WindowCloseEvent &e);
     };
 
 }
