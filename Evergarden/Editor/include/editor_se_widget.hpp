d77f2a18d5ab889dac05c97f2a3f03138bea7761
Chenghua <68260701+chenghuaWang@users.noreply.github.com>
2022-04-29 17:56:41 +0800

    Create editor_se_widget.hpp

---
+++ Evergarden/Editor/include/editor_se_widget.hpp
@@ -0,0 +1,28 @@
+#ifndef H_EDITOR_SE_WIDGET
+#define H_EDITOR_SE_WIDGET
+
+#ifdef  WIN32
+#pragma once
+#endif //! WIN32
+
+#include <imgui/TextEditor.h>
+
+namespace evergarden {
+
+    class App;
+
+    class Widget_ShaderEditor {
+    public:
+        Widget_ShaderEditor() = default;
+        ~Widget_ShaderEditor() = default;
+        void init();
+        void onGuiRender();
+
+    private:
+        TextEditor                  m_editor;
+        TextEditor::Coordinates     m_cursor_pos;
+    };
+
+}
+
+#endif //! H_EDITOE_SE_WIDGET
