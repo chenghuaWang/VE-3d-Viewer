201386b0a7b88873b203050dc580c1ce8350848f
Chenghua <68260701+chenghuaWang@users.noreply.github.com>
2022-04-29 11:49:12 +0800

    Create editor_core.hpp

---
+++ Evergarden/Editor/include/editor_core.hpp
@@ -0,0 +1,25 @@
+/*!
+ * @author  chenghua.wang
+ * @file    Evergarden/Editor/include/editor_core.hpp
+ * @brief   Include all head files that application needed.
+ */
+
+#ifndef H_EDITOR_CORE
+#define H_EDITOR_CORE
+
+#ifdef  WIN32
+#pragma once
+#endif  //! WIN32
+
+#include "app_window.hpp"
+
+#include "editor_layer.hpp"
+
+#include "editor_file_utils.hpp"
+#include "editor_file_widget.hpp"
+#include "editor_log_layer.hpp"
+#include "editor_main_layer.hpp"
+#include "editor_scene_layer.hpp"
+#include "editor_se_layer.hpp"
+
+#endif //! H_EDITOR_CORE
