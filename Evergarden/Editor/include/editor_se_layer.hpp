df92db44de1060494fe28aed5baa4f9e4045cd07
Chenghua <68260701+chenghuaWang@users.noreply.github.com>
2022-04-29 17:56:45 +0800

    Update editor_se_layer.hpp

--- Evergarden/Editor/include/editor_se_layer.hpp
+++ Evergarden/Editor/include/editor_se_layer.hpp
@@ -1,1 +1,39 @@
-// Shader editor layer
+/*!
+ * @author  chenghua.wang
+ * @file    Evergarden/Editor/include/editor_se_layer.hpp
+ * @brief   Shader editore layer.
+ */
+
+#ifndef H_EDITOR_SE_LAYER
+#define H_EDITOR_SE_LAYER
+
+#ifdef  WIN32
+#pragma once
+#endif
+
+#include "editor_layer.hpp"
+#include "editor_se_widget.hpp"
+
+namespace evergarden {
+
+    class Layer_ShaderEditor: public Layer {
+    public:
+        Layer_ShaderEditor();
+        ~Layer_ShaderEditor() override = default;
+
+    public:
+        void onAttach()         override {};
+        void onDetach()         override {};
+        void onUpdate()         override {};
+        void onGuiRender()      override;
+        void onEvent(Event& e)  override {};
+
+    private:
+        bool                    m_window_on = true;
+        Widget_ShaderEditor     m_se {};
+    };
+
+}
+
+#endif
+
