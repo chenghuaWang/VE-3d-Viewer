1ae64d7e300cbd9edcb61343ff1b4909f2cccb15
Chenghua <68260701+chenghuaWang@users.noreply.github.com>
2022-04-29 11:49:23 +0800

    Update editor_layer.cpp

---
+++ Evergarden/Editor/src/editor_layer.cpp
@@ -0,0 +1,124 @@
+/*!
+ * @author  chenghua.wang
+ * @file    Evergarden/Editor/src/editor_layer.cpp
+ * @brief   The most basic class for all layers.
+ */
+
+#include "../include/editor_layer.hpp"
+
+namespace evergarden {
+
+    LayerStack::~LayerStack() {
+        for (auto item:m_stack) { delete item; };
+    }
+
+    void LayerStack::pushLayer(Layer* la) {
+        la->__setIdxInStack__((int)m_cur_idx);
+        m_stack.emplace(m_stack.begin() + m_cur_idx, la);
+        ++ m_cur_idx;
+    }
+
+    void LayerStack::popLayer(Layer* la) {
+        auto it = std::find(m_stack.begin(), m_stack.end(), la);
+        if (it != m_stack.end()) {
+                la->onDetach();
+                m_stack.erase(it);
+                -- m_cur_idx;
+        }
+    }
+
+}
+
+namespace evergarden {
+
+    ImGuiLayer::ImGuiLayer(std::shared_ptr<MainWindow> window): Layer("in_imgui"), m_window(window) {};
+
+    ImGuiLayer::~ImGuiLayer() {};
+
+    void ImGuiLayer::onAttach() {
+        IMGUI_CHECKVERSION();
+        ImGui::CreateContext();
+        ImGuiIO& io = ImGui::GetIO(); (void)io;
+        io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;
+        io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;
+        io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
+
+        ImGui::StyleColorsDark();
+
+        ImGuiStyle& style = ImGui::GetStyle();
+        if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+            style.WindowRounding = 0.0f;
+            style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+        }
+
+        io.Fonts->AddFontFromFileTTF(get_abs_path("/Asset/font/JetBrainsMono-Medium.ttf").c_str(), 20.0f);
+
+        GLFWwindow* window = static_cast<GLFWwindow*>(m_window->getPlatformWindow());
+
+        ImGui_ImplGlfw_InitForOpenGL(window, true);
+        ImGui_ImplOpenGL3_Init("#version 410");
+    }
+
+    void ImGuiLayer::onDetach() {
+        ImGui_ImplOpenGL3_Shutdown();
+        ImGui_ImplGlfw_Shutdown();
+        ImGui::DestroyContext();
+    }
+
+    void ImGuiLayer::begin() {
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGuizmo::BeginFrame();
+        ImGuizmo::Enable(true);
+
+        static bool p_open = true;
+        static bool opt_fullscreen_persistant = true;
+        bool opt_fullscreen = opt_fullscreen_persistant;
+        static ImGuiDockNodeFlags dockspace_flags = ImGuiDockNodeFlags_None;
+
+        ImGuiWindowFlags window_flags = ImGuiWindowFlags_MenuBar | ImGuiWindowFlags_NoDocking;
+        if (opt_fullscreen) {
+            ImGuiViewport* viewport = ImGui::GetMainViewport();
+            ImGui::SetNextWindowPos(viewport->WorkPos);
+            ImGui::SetNextWindowSize(viewport->WorkSize);
+            ImGui::SetNextWindowViewport(viewport->ID);
+            ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
+            ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+            window_flags |= ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
+            window_flags |= ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus;
+        }
+        if (dockspace_flags & ImGuiDockNodeFlags_PassthruCentralNode)
+            window_flags |= ImGuiWindowFlags_NoBackground;
+        ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
+        ImGui::Begin("DockSpace", &p_open, window_flags);
+        ImGui::PopStyleVar();
+
+        if (opt_fullscreen) ImGui::PopStyleVar(2);
+
+        ImGuiIO& io = ImGui::GetIO();
+        if (io.ConfigFlags & ImGuiConfigFlags_DockingEnable) {
+            ImGuiID dockspace_id = ImGui::GetID("DockSpace");
+            ImGui::DockSpace(dockspace_id, ImVec2(0.0f, 0.0f), dockspace_flags);
+        }
+    }
+
+    void ImGuiLayer::end() {
+        ImGui::End();
+
+        ImGuiIO& io = ImGui::GetIO();
+        io.DisplaySize = ImVec2((float)m_window->W(), (float)m_window->H());
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+            GLFWwindow* backup_current_context = glfwGetCurrentContext();
+            ImGui::UpdatePlatformWindows();
+            ImGui::RenderPlatformWindowsDefault();
+            glfwMakeContextCurrent(backup_current_context);
+        }
+    }
+
+}
