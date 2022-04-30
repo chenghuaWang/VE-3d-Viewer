2bbc40e8ffba5228d1f33a4d11d0be341f47314f
Chenghua <68260701+chenghuaWang@users.noreply.github.com>
2022-04-30 15:55:49 +0800

    Add scene class

---
+++ Evergarden/Renderer/include/render_scene.hpp
@@ -0,0 +1,20 @@
+/*!
+ * @author  chenghua.wang
+ * @file    Evergarden/Rednerer/include/renderer_scene.hpp
+ * @brief   A API package for scene class.
+ */
+
+#ifndef H_RENDER_SCENE
+#define H_RENDER_SCENE
+
+#ifdef  WIN32
+#pragma once
+#endif //! WIN32
+
+namespace evergarden {
+
+    
+
+} //! namespace evergarden
+
+#endif //! H_RENDER_SCENE
