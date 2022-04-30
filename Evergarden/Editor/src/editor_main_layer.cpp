7024ea003333337568ef63849a21db30ab54221f
Chenghua <68260701+chenghuaWang@users.noreply.github.com>
2022-04-30 15:53:22 +0800

    Update editor_main_layer.cpp

---
+++ Evergarden/Editor/src/editor_main_layer.cpp
@@ -0,0 +1,20 @@
+/*!
+ * @author  chenghua.wang
+ * @file    Evergarden/Editor/src/editor_main_layer.cpp
+ * @brief   Main Layer contain multi layers.
+ */
+
+#include "../include/editor_main_layer.hpp"
+
+namespace evergarden {
+
+    Layer_Main::Layer_Main(): Layer("Main Layer") {
+
+    };
+
+    void Layer_Main::onGuiRender() {
+        
+    }
+
+}
+
