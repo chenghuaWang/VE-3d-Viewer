a07a430b1dd7c6b390c46ef659b32e3fff7c48e0
Chenghua <68260701+chenghuaWang@users.noreply.github.com>
2022-04-30 15:56:59 +0800

    Create render_base.cpp

---
+++ Evergarden/Renderer/src/render_base.cpp
@@ -0,0 +1,23 @@
+/*!
+ * @author  chenghua.wang
+ * @file    Evergarden/Renderer/include/render_base.hpp
+ * @brief   Base file for Renderer. Provide macros and basic functions
+ * 
+ * @details We packagged OpenGL API in this Class-like interface.
+ */
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "../include/render_base.hpp"
+
+void GLClearError() {
+    while (glGetError() != GL_NO_ERROR);
+}
+
+bool GLLogCall(const char* function, const char* file, int line) {
+    while (GLenum error = glGetError()) {
+        std::cout << "[Opengl Error] (" << error << "):" << function << " " <<
+                  file << ":" << line << std::endl;
+        return false;
+    }
+    return true;
+}
