3163f4700cebbd2390a6f0da1bb0bddd42fc2469
Chenghua <68260701+chenghuaWang@users.noreply.github.com>
2022-05-02 09:34:44 +0800

    no need?

---
+++ Evergarden/Renderer/include/render_entity.hpp
@@ -0,0 +1,23 @@
+/*!
+ * @author  chenghua.wang
+ * @file    Evergarden/Rednerer/include/renderer_entity.hpp
+ * @brief   A part of entity component system.
+ */
+
+#ifndef H_RENDER_ENTITY
+#define H_RENDER_ENTITY
+
+#ifdef WIN32
+#pragma once
+#endif //! WIN32
+
+#include <entt/entt.hpp>
+#include "render_base.hpp"
+#include "render_scene.hpp"
+
+namespace evergarden{
+    
+} // namespace evergarde
+
+
+#endif //! H_RENDER_ENTITY
